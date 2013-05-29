aa354dc5ec3f0bd302654af8ac36e3dea9dd86ea
Captain Shadows <captain_shadows@aol.com>
2013-05-29 19:15:18 -0400

    Updated Nucleum

--- common/ccm/nucleum_omnium/configuration/PropertyHandler.java
+++ common/ccm/nucleum_omnium/configuration/PropertyHandler.java
@@ -1,6 +1,5 @@
 package ccm.nucleum_omnium.configuration;
 
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -8,37 +7,23 @@ import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraftforge.common.Configuration;
 import net.minecraftforge.common.Property;
-import ccm.nucleum_omnium.IMod;
 
 public class PropertyHandler
 {
 
-    private final int                             baseItemID;
+    private int           baseItemID;
 
-    private final int                             baseBlockID;
+    private int           baseBlockID;
 
-    private static HashMap<IMod, Boolean>         hasPropertyHandler = new HashMap<IMod, Boolean>();
+    private List<Integer> freeItemIDs  = new LinkedList<Integer>();
 
-    private static HashMap<IMod, PropertyHandler> idHandler          = new HashMap<IMod, PropertyHandler>();
+    private List<Integer> freeBlockIDs = new LinkedList<Integer>();
 
-    private final List<Integer>                   freeItemIDs        = new LinkedList<Integer>();
-
-    private final List<Integer>                   freeBlockIDs       = new LinkedList<Integer>();
-
-    private PropertyHandler(final int baseItemID,
-                            final int baseBlockID)
+    public PropertyHandler(final int baseItemID,
+                           final int baseBlockID)
     {
         this.baseItemID = baseItemID;
         this.baseBlockID = baseBlockID;
-    }
-
-    public static PropertyHandler getConfigInstance(final IMod requester, final int baseItemID, final int baseBlockID)
-    {
-        if (!hasPropertyHandler.containsKey(requester)){
-            hasPropertyHandler.put(requester, true);
-            idHandler.put(requester, new PropertyHandler(baseItemID, baseBlockID));
-        }
-        return idHandler.get(requester);
     }
 
     /**
