230a06b5888ae831f51ccc9f2cbb840bb9ea0713
Captain Shadows <captain_shadows@aol.com>
2013-06-14 17:17:56 -0400

    Fixed versions

--- common/ccm/nucleum_omnium/NucleumOmnium.java
+++ common/ccm/nucleum_omnium/NucleumOmnium.java
@@ -48,6 +48,7 @@ public class NucleumOmnium extends DummyModContainer implements IMod {
         final ModMetadata meta = getMetadata();
         meta.modId = Archive.MOD_ID;
         meta.name = Archive.MOD_NAME;
+        meta.version = "@VERSION@";
         meta.authorList = Arrays.asList("Captain_Shadows");
         meta.url = "https://github.com/CCM-Modding/Nucleum-Omnium";
         meta.updateUrl = "http://driesgames.game-server.cc:8080/view/CCM/job/Nucleum-Omnium/";
