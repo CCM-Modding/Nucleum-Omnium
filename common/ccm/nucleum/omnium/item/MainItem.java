92ce16ccdb7695d937ea8537cb29e99b26842460
Captain Shadows <captain_shadows@aol.com>
2013-08-19 15:28:42 -0400

    ...

---
+++ common/ccm/nucleum/omnium/item/MainItem.java
@@ -0,0 +1,26 @@
+/**
+ * CCM Modding, Nucleum-Omnium
+ */
+package ccm.nucleum.omnium.item;
+
+import net.minecraft.item.Item;
+
+/**
+ * MainItem
+ * <p>
+ * 
+ * @author Captain_Shadows
+ */
+public class MainItem extends Item
+{
+
+    /**
+     * @param id
+     */
+    public MainItem(final int id)
+    {
+        super(id);
+        // TODO Auto-generated constructor stub
+    }
+
+}
