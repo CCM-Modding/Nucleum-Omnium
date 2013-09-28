65c0166955d1004d7ab41f773c4ee4486a03a1d9
Captain Shadows <captain_shadows@aol.com>
2013-09-27 20:47:57 -0400

    Coremod stuffs

--- common/ccm/nucleum/omnium/asm/OmniumModContainer.java
+++ common/ccm/nucleum/omnium/asm/OmniumModContainer.java
@@ -3,25 +3,15 @@
  */
 package ccm.nucleum.omnium.asm;
 
-import java.io.File;
-import java.security.cert.Certificate;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-import com.google.common.eventbus.EventBus;
-
-import cpw.mods.fml.common.LoadController;
+import cpw.mods.fml.common.DummyModContainer;
 import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.MetadataCollection;
-import cpw.mods.fml.common.ModContainer;
 import cpw.mods.fml.common.ModMetadata;
 import cpw.mods.fml.common.versioning.ArtifactVersion;
 import cpw.mods.fml.common.versioning.DefaultArtifactVersion;
 import cpw.mods.fml.common.versioning.VersionRange;
 
-public class OmniumModContainer implements ModContainer
+public class OmniumModContainer extends DummyModContainer
 {
 
     private ModMetadata md = new ModMetadata();
@@ -45,30 +35,6 @@ public class OmniumModContainer implements ModContainer
     public void bindMetadata(final MetadataCollection mc)
     {
         md = mc.getMetadataForId(getModId(), null);
-    }
-
-    @Override
-    public Map<String, String> getCustomModProperties()
-    {
-        return EMPTY_PROPERTIES;
-    }
-
-    @Override
-    public Class<?> getCustomResourcePackClass()
-    {
-        return null;
-    }
-
-    @Override
-    public List<ArtifactVersion> getDependants()
-    {
-        return Collections.emptyList();
-    }
-
-    @Override
-    public List<ArtifactVersion> getDependencies()
-    {
-        return Collections.emptyList();
     }
 
     @Override
@@ -112,30 +78,6 @@ public class OmniumModContainer implements ModContainer
     }
 
     @Override
-    public Set<ArtifactVersion> getRequirements()
-    {
-        return Collections.emptySet();
-    }
-
-    @Override
-    public Certificate getSigningCertificate()
-    {
-        return null;
-    }
-
-    @Override
-    public String getSortingRules()
-    {
-        return "";
-    }
-
-    @Override
-    public File getSource()
-    {
-        return null;
-    }
-
-    @Override
     public String getVersion()
     {
         return md.version;
@@ -145,33 +87,5 @@ public class OmniumModContainer implements ModContainer
     public boolean isImmutable()
     {
         return true;
-    }
-
-    @Override
-    public boolean isNetworkMod()
-    {
-        return false;
-    }
-
-    @Override
-    public boolean matches(final Object mod)
-    {
-        return false;
-    }
-
-    @Override
-    public boolean registerBus(final EventBus bus, final LoadController controller)
-    {
-        return false;
-    }
-
-    @Override
-    public void setEnabledState(final boolean enabled)
-    {}
-
-    @Override
-    public String toString()
-    {
-        return md != null ? getModId() : "Dummy Container (" + label + ") @" + System.identityHashCode(this);
     }
 }
