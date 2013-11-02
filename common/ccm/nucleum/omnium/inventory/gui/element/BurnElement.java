61bd08a838e1ab07091a0c919c64836aa472a4a6
Captain Shadows <captain_shadows@aol.com>
2013-11-01 20:45:30 -0400

    elements

---
+++ common/ccm/nucleum/omnium/inventory/gui/element/BurnElement.java
@@ -0,0 +1,25 @@
+package ccm.nucleum.omnium.inventory.gui.element;
+
+import lib.cofh.gui.GuiBase;
+import lib.cofh.gui.element.ElementBase;
+
+public class BurnElement extends ElementBase
+{
+
+    public BurnElement(GuiBase gui, int posX, int posY)
+    {
+        super(gui, posX, posY);
+    }
+
+    @Override
+    public void draw()
+    {
+        
+    }
+
+    @Override
+    public String getTooltip()
+    {
+        return null;
+    }
+}
